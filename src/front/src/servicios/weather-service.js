ac968d1a242a5877e6e2e153eff5415a7a0aab2e
Sofiaivars <sofiaivars1@gmail.com>
2025-09-17 11:46:14 +0200

    back astroradar

--- src/front/src/servicios/weather-service.js
+++
@@ -1,7 +0,0 @@
-const getWeather = async (latitud, longitud) => {
-  const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&current=temperature_2m,precipitation,rain,is_day,cloud_cover,weather_code,relative_humidity_2m,wind_speed_10m&timezone=auto`);
-  const data = await response.json();
-  return { current: data.current, current_units: data.current_units };
-}
-
-export { getWeather };
