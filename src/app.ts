4b0a026e18e367f41b3e40e7badf61dc4cc8cd7c
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-11-25 12:30:35 +0100

    configuración inicial y alias para rutas

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('test');
+import { envs } from "./config/envs";
+import { Server } from "./config/server";
+
+
+(async () => {
+  main();
+})();
+
+function main() {
+  const server = new Server({
+    port: envs.PORT,
+    routes: ""
+  });
+}
